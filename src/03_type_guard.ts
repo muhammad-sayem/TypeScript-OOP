f1f19efd26384e1914876f2838bd460da1a25a04
muhammad-sayem <shahrulislamsayem@gmail.com>
2025-11-14 23:37:19 +0600

    Type guard in

---
+++ src/03_type_guard.ts
@@ -0,0 +1,54 @@
+type Alphanumeric = number | string
+
+const add = (num1: Alphanumeric, num2: Alphanumeric) => {
+  if (typeof num1 === "number" && typeof num2 === "number") {
+    return num1 + num2;
+  }
+  else {
+    return num1.toString() + num2.toString();
+  }
+}
+
+add(2, 2) // 4
+add(2, "2") // 22
+add('2', 2) // 22
+add('2', "2") // 22
+
+// ------------------------------------ //
+
+// In type guard //
+type NormalUser = {
+  name: string
+}
+
+type SpecialUser = {
+  name: string;
+  role: string;
+}
+
+const getUserInfo = (user: NormalUser | SpecialUser) => {
+  if ("role" in user) {
+    console.log(`The user is ${user.name} and his role is ${user.role}`);
+  }
+  else {
+    console.log(`This user is ${user.name}`);
+  }
+}
+
+const user1: NormalUser = {
+  name: "Mir"
+}
+
+const user2: SpecialUser = {
+  name: "Mezba",
+  role: "Moderator"
+}
+
+getUserInfo(user1);
+getUserInfo(user2);
+
+/*
+  Ekhane ekhon type guard er maddhome emon kora hoise je shudhu name 
+  pass korle NormalUser er type er ta print hobe ar name and role pathale
+  SpecialUser er ta print hobe
+*/
